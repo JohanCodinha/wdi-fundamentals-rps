4de3486693a59293018bfd53f43b21d8afa5846e
Johan Codinha <johancodinha@PanelMBA.local>
2016-03-08 13:49:04 +1100

    getWinner function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -43,7 +43,12 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
-    return winner;
+    if (playerMove === computerMove) return "tie";
+        else if (playerMove === "rock" && computerMove === "scissors") return "player";
+        else if (playerMove === "scissors" && computerMove === "paper") return "player";
+        else if (playerMove === "paper" && computerMove === "rock") return "player";
+        else return "computer";
+    //return winner;
 }
 
 function playToFive() {
